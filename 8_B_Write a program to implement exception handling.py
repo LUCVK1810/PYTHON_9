e47f260f8f5922edb7bf8c5ff98cc500b9fcd960
LUCVK1810 <lucuniversal101@gmail.com>
2024-09-15 16:44:45 +0530

    Add files via upload

---
+++ 8_B_Write a program to implement exception handling.py
@@ -0,0 +1,7 @@
+#8_B_Write a program to implement exception handling
+try:
+    A = int(input("Enter a:"))
+    B = int(input("Enter b:"))
+    C = a/b 
+except:
+    print("Can’t divide with zero") 
